7f445754dc8d4a0955130cc4929ca7ff5c41d54e
Lucas Borel <lucasbmoura32@gmail.com>
2024-09-23 11:26:18 -0300

    Front-End finalizado

---
+++ app/src/main/java/com/guilhermedelecrode/appmatch/Detalhes_Vaga_FreeActivity.kt
@@ -0,0 +1,20 @@
+package com.guilhermedelecrode.appmatch
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Detalhes_Vaga_FreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes_vaga_free)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
