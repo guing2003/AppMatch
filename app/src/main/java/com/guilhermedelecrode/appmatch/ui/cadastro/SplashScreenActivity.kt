bf263bb5538c42d410ae151802290ed97c1577b3
guing2003 <guilhermedelecrode1710@gmail.com>
2025-04-25 10:15:29 -0300

    Adiciona Animação na tela de SplashScreen

--- app/src/main/java/com/guilhermedelecrode/appmatch/ui/cadastro/SplashScreenActivity.kt
+++ app/src/main/java/com/guilhermedelecrode/appmatch/ui/cadastro/SplashScreenActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.guilhermedelecrode.appmatch.common.AbstractActivity
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.guilhermedelecrode.appmatch.MainActivity
 import com.guilhermedelecrode.appmatch.R
+import com.guilhermedelecrode.appmatch.common.AbstractActivity
 
 class SplashScreenActivity : AbstractActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +17,14 @@ class SplashScreenActivity : AbstractActivity() {
 
         configActionBarLogin()
 
+        val logoImage = findViewById<ImageView>(R.id.logo_image_splash_screen_activity)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.splash_screen_anim)
+        logoImage.startAnimation(animation)
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }, 3000)
     }
 }
