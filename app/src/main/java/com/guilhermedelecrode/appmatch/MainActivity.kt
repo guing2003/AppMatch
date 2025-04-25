32bb036acaece3987f599865de8b8f4af5c61a45
guing2003 <guilhermedelecrode1710@gmail.com>
2025-04-25 10:01:33 -0300

    Adiciona Melhorias Visuais Na tela de Login

--- app/src/main/java/com/guilhermedelecrode/appmatch/MainActivity.kt
+++ app/src/main/java/com/guilhermedelecrode/appmatch/MainActivity.kt
@@ -29,13 +29,13 @@ class MainActivity : AbstractActivity() {
 
         auth = Firebase.auth
 
-        val btn_login = findViewById<Button>(R.id.btn_login_acitivity_main)
+        val btn_login = findViewById<Button>(R.id.btn_login_activity_main)
 
         btn_login.setOnClickListener {
             val email: String =
-                findViewById<EditText>(R.id.edit_email_acitivity_main).text.toString()
+                findViewById<EditText>(R.id.edit_email_activity_main).text.toString()
             val senha: String =
-                findViewById<EditText>(R.id.edit_senha_acitivity_main).text.toString()
+                findViewById<EditText>(R.id.edit_senha_activity_main).text.toString()
 
             if (email.isNotEmpty() && senha.isNotEmpty()) {
                 signInWithEmailAndPassword(email, senha)
@@ -48,17 +48,17 @@ class MainActivity : AbstractActivity() {
             }
         }
 
-        val txt_Cadastro = findViewById<TextView>(R.id.txt_cadastro_acitivity_main)
+        val txt_Cadastro = findViewById<TextView>(R.id.txt_cadastro_activity_main)
         txt_Cadastro.setOnClickListener {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
         }
 
-        val txt_recuperar_senhaa: TextView = findViewById(R.id.txt_recuperar_senha_acitivity_main)
+        val txt_recuperar_senhaa: TextView = findViewById(R.id.txt_recuperar_senha_activity_main)
         txt_recuperar_senhaa.setOnClickListener {
             Log.d("Listener", "Clicou no listener")
             val email: String =
-                findViewById<EditText>(R.id.edit_email_acitivity_main).text.toString()
+                findViewById<EditText>(R.id.edit_email_activity_main).text.toString()
             if (email.isNotEmpty()) {
                 sendPasswordResetEmail(email)
                 Log.d("Email", "Email: ${email}")
